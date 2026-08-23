50b1e49a9c722a3f99d85dcd50d4605ebe9c1e11
LinzhouZZZZZ <albertzhaolinzhou@gmail.com>
2026-08-23 19:10:38 +0930

    Align database name for MySQL setup

--- sql/00_create_database.sql
+++ sql/00_create_database.sql
@@ -1,5 +1,8 @@
 -- 00_create_database.sql
--- MySQL 8+
+-- MySQL 8.4+
 
-CREATE DATABASE IF NOT EXISTS ecommerce_analytics;
-USE ecommerce_analytics;
+CREATE DATABASE IF NOT EXISTS ecommerce_da
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci;
+
+USE ecommerce_da;
